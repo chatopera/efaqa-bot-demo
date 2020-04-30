a3aee2f7bdc6c7c39e154b10f0131a7244c51f12
Hai Liang Wang <hain@chatopera.com>
2020-04-30 22:19:37 +0800

    kick off

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # efaqa-bot-demo
+
 心理问答机器人示例程序
+
+## TL;DR
+
+```
+cd app
+npm install
+cp app/sample.env app/.env # edit .env, add client id and secret
+../admin/start.sh
+open app/demo.js
+```
+
+## 使用文档
+
+[文档](https://docs.chatopera.com/products/psych-assistant/api.html)
+
+## 其它链接
+
+[Chatopera 云服务](https://bot.chatopera.com)
+[Chatopera Node.js SDK](https://www.npmjs.com/package/@chatopera/sdk)
+
+## 开源许可协议
+
+Copyright (2018-2020) <a href="https://www.chatopera.com/" target="_blank">北京华夏春松科技有限公司</a>
+
+[MIT](https://github.com/chatopera/efaqa-bot-demo/blob/master/LICENSE)
+
+[![chatoper banner][co-banner-image]][co-url]
+
+[co-banner-image]: https://user-images.githubusercontent.com/3538629/42383104-da925942-8168-11e8-8195-868d5fcec170.png
+[co-url]: https://www.chatopera.com
---
+++ app/demo.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+// -*- coding: utf-8 -*-
+//===============================================================================
+//
+// Copyright (c) 2020 <> All Rights Reserved
+//
+//
+// File: /Users/hain/chatopera/efaqa-bot-demo/app/demo.js
+// Author: Hai Liang Wang
+// Date: 2020-04-30:21:34:01
+//
+//===============================================================================
+/**
+ *
+ */
+require("dotenv").config();
+const argv = process.argv;
+const curdir = __dirname;
+const debug = require("debug")("demo");
+const path = require("path");
+const samples = require(path.join(curdir, "..", "assets", "sample.json"));
+const Chatbot = require("@chatopera/sdk");
+
+const CLIENT_ID = process.env["CLIENT_ID"];
+const CLIENT_SECRET = process.env["CLIENT_SECRET"];
+const DEMO_FLOW_CHAT = process.env["DEMO_FLOW_CHAT"] || "on";
+const DEMO_FLOW_SEARCH = process.env["DEMO_FLOW_SEARCH"] || "on";
+
+const bot = new Chatbot(CLIENT_ID, CLIENT_SECRET);
+
+/**
+ * 调用查询接口
+ */
+async function search() {
+  for (let x of samples.search.data) {
+    let resp = await bot.psychSearch(x, samples.search.threshold);
+    debug("\n\nsearch input: %s", x);
+    debug("bot response: \n%s", JSON.stringify(resp, null, " "));
+    debug("***********************");
+  }
+}
+
+/**
+ * 调用聊天接口
+ */
+async function chat() {
+  // chat为多轮对话，每个会话有20分钟有效期，在会话期内查询上下文
+  // 强制清除会话状态，发送 @SESSION_DESTROY@
+  // 本程序是演示，测试，在演示前强制刷新会话，实际使用中不必调用
+  await bot.psychChat(
+    samples.chat.channel,
+    samples.chat.channelId,
+    samples.chat.userId,
+    "@SESSION_DESTROY@"
+  );
+
+  // 开始对话
+  for (let x of samples.chat.data) {
+    let resp = await bot.psychChat(
+      samples.chat.channel,
+      samples.chat.channelId,
+      samples.chat.userId,
+      x
+    );
+    debug("\n\nchat input: %s", x);
+    debug("bot response: \n%s", JSON.stringify(resp, null, " "));
+    debug("***********************");
+  }
+}
+
+// main function
+async function main() {
+  debug("bot: client id %s, secret ********", CLIENT_ID);
+  if (DEMO_FLOW_SEARCH.toLocaleLowerCase() == "on") await search();
+  if (DEMO_FLOW_CHAT.toLocaleLowerCase() == "on") await chat();
+}
+
+// on main entry
+if (require.main === module) {
+  (async function () {
+    await main();
+  })();
+}
